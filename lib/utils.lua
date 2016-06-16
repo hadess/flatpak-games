hadess/flatpak-games

commit ea9d27a1a4050d222408d8a6685954d58ea478ee

utils: Check if path is to a file in file_exists()
Otherwise we could be checking for a directory, or another else that
does "exist".

lib/utils.lua
@@ -69,5 +69,5 @@ function file_exists(path)
 	if not stat then
 		return false
 	end
-	return true
+	return S_ISREG(stat)
 end

commit f5cefc8f3835a6ed7e930cdd8155b17da0357b08

utils: Fix file_exists() not working
Bug introduced in ea9d27a. This time, we're also adding regression
tests.

lib/utils.lua
@@ -69,5 +69,5 @@ function file_exists(path)
 	if not stat then
 		return false
 	end
-	return S_ISREG(stat)
+	return stat.type == 'regular'
 end

run-tests.lua
@@ -34,3 +34,7 @@ assert(identify('tests/lugaru-full-linux-x86-1.0c.bin-header') == 'mojo')
 -- Test the reverse DNS
 assert(reverse_dns('gog.com') == 'com.gog')
 assert(reverse_dns('wolfire.com') == 'com.wolfire')
+
+-- Test file exists
+assert(file_exists('run-tests.lua'))
+assert(not file_exists('DOES NOT EXIST.lua'))